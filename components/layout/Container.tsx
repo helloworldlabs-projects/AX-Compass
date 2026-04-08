15268eed7cd59ec76ba111d10f0340142e520cd4
goldenbat <goldenbat@helloworldlabs.kr>
2026-04-08 17:45:26 +0900

    feat: Container, Section 레이아웃 컴포넌트 추가

---
+++ components/layout/Container.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Container({ children, className }: ContainerProps) {
+  return (
+    <div
+      className={cn(
+        'bg-special-navy-0 relative flex w-full flex-col items-center gap-[50px] rounded-[20px] px-2.5 lg:gap-[75px] lg:rounded-[50px]',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ components/layout/Section.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils';
+
+interface SectionProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Section({ children, className }: SectionProps) {
+  return (
+    <div
+      className={cn('relative flex w-full flex-col items-center gap-[50px] py-[50px]', className)}
+    >
+      {children}
+    </div>
+  );
+}
