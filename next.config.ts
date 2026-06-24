b3c16cdf1dc0e1d515c637cedc7b7a1f33e0bf39
goldenbat <goldenbat@helloworldlabs.kr>
2026-06-24 11:48:07 +0900

    feat: 대시보드 API 레이어 구현

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.0.9'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.safarion.co.kr',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hwl-safarion-bucket.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hwl-safarion-dev-bucket.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
