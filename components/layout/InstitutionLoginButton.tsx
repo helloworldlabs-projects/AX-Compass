a56fa53a130aebbf75e08a5a8e93945e233d99ee
goldenbat <goldenbat@helloworldlabs.kr>
2026-06-25 15:39:34 +0900

    fix(ui): 기관 로그인 문구 수정

--- components/layout/InstitutionLoginButton.tsx
+++ components/layout/InstitutionLoginButton.tsx
@@ -72,8 +72,7 @@ export default function InstitutionLoginButton() {
         onClick={() => setOpen(true)}
       >
         <Building className="size-5" />
-        {/* <span className="txt-b-bold">기관 로그인 & 회원가입</span> */}
-        <span className="txt-b-bold">기관 로그인</span>
+        <span className="txt-b-bold">기관 로그인 & 회원가입</span>
       </button>
       <AdminLoginModal open={open} onClose={() => setOpen(false)} onConfirm={handleConfirm} />
     </>
